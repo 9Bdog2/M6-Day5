25fad24a71f5345e8243ff2079cba33445a5ad6f
Bogdan Birau <birau.bogdan92@gmail.com>
2021-12-17 09:10:15 +0100

    Update server.js

--- homework/src/server.js
+++ homework/src/server.js
@@ -5,6 +5,7 @@ import productsRouter from "./services/products/index.js";
 import reviewsRouter from "./services/reviews/index.js";
 import categoriesRouter from "./services/categories/index.js";
 import models from "./services/products/index.js";
+import listEndpoints from "express-list-endpoints";
 
 const server = express();
 
@@ -19,6 +20,7 @@ server.use("/categories", categoriesRouter);
 
 server.listen(PORT, async () => {
   console.log(`Server is listening on port ${PORT}`);
+  console.log(listEndpoints(server));
   await testConnection();
   await connectDB();
 });
